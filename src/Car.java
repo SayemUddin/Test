1a41c2b37c01eb68e57ae63eb562b328293fe194
SayemUddin <c201127@ugrad.iiuc.ac.bd>
2021-10-14 00:29:12 +0600

    EDITED

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,8 @@
+
 public class Car {
     public static void main (String[]args){
-        System.out.println("Corolla 2006");
+        System.out.println("COROLLA 2006");
+        System.out.println("ALLION 2011");
+        System.out.println("Output Edited");
     }
 }
